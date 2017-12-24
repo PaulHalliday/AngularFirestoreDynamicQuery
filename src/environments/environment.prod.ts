39cee045771e1ce9bfee56bca541a83383f6c213
PaulHalliday <hello@paulhalliday.io>
2017-12-24 19:31:14 +0000

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   firebase: {
+    apiKey: 'AIzaSyDzCewTRlEAJaMlwexTmiFrLd8xkXznmv0',
+    authDomain: 'angularfirebasetutorial-e3db9.firebaseapp.com',
+    databaseURL: 'https://angularfirebasetutorial-e3db9.firebaseio.com',
+    projectId: 'angularfirebasetutorial-e3db9',
+    storageBucket: 'angularfirebasetutorial-e3db9.appspot.com',
+    messagingSenderId: '620928996380'
   },
   production: true
 };
